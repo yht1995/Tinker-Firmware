0a736e803f21bb556bc4bb6b534d356cbf30d44c
yaoht <yht199505@gmail.com>
2015-10-20 19:26:26 +0800

    [ADD] CRC Utility

---
+++ source/motor/CRC/CRC.h
@@ -0,0 +1,18 @@
+#ifndef __CRC_H_
+#define __CRC_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+
+uint16_t CRC16(char* pushMsg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
